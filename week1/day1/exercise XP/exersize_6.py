011393f4eeb6b49fb93ce03cb224c8e844da777e
Wolfasaurus-08 <cordelia86@gmail.com>
2025-02-02 16:59:16 +0200

    sixth_exersize

---
+++ week1/day1/exercise XP/exersize_6.py
@@ -0,0 +1,11 @@
+#Exercise 6 : A & B
+#Instructions
+#Create two variables, a and b.
+#Each variable value should be a number.
+#If a is bigger then b, have your code print Hello World.
+a=14
+b=102
+if a>b:
+    print ("Hello World")
+else:
+    print("banana")
