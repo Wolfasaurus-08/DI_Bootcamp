50b4484b9a2bbb32029890c77b7ffbe67024b476
Wolfasaurus-08 <cordelia86@gmail.com>
2025-02-02 20:44:50 +0200

    eigth_exersize

---
+++ week1/day1/exercise XP/exercize_8.py
@@ -0,0 +1,9 @@
+#Exercise 8 : What’s your name ?
+#Instructions
+#Write code that asks the user for their name and determines whether or not you have the same name, print out a funny message based on the outcome.
+my_name = "Anne"
+user_name = input("write your name ")
+if user_name == my_name:
+    print("hey! get your own name")
+else:
+    print ("amazing appellation")
